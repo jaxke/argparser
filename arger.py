fc34cf48acf8dc2b6d2bfeba5abe75cacae97720
JS <jandede@protonmail.com>
2019-12-25 04:02:14 +0300

    arrays are now accepted, custom exception raised instead of sys.exit, multiple flags can be used for one argument

--- arger.py
+++ arger.py
@@ -5,44 +5,58 @@ import sys
 class Arger:
     sys_args = []
     args_parsed = []
+    accepted_flags = []
 
     def __init__(self):
         self.sys_args = sys.argv
 
     def add_arg(self, *argv, store_true=False, help=""):
-        if len(argv) > 1:
-            raise Exception
-        self.args_parsed.append(Argument(argv[0], store_true, help=""))
+        for arg in argv:
+            if arg in self.accepted_flags:
+                raise ArgumentException("[Arger] Multiple different arguments try to use the flag " + arg)
+        self.args_parsed.append(Argument(argv, store_true, help))
+        for a in argv:
+            self.accepted_flags.append(a)
 
+    # TODO object boolean attribute to accept arrays OR NOT?
     def parse(self):
         for i, sys_arg in enumerate(self.sys_args):
-            for arg_parsed in self.args_parsed:
-                if arg_parsed.name == sys_arg:
+            if sys_arg[0] == "-" and sys_arg not in self.accepted_flags:
+                # TODO this is more of a developer orientated exception message?
+                raise ArgumentException("[Arger] " + sys_arg + " was not added and therefore not accepted.")
+            for j, arg_parsed in enumerate(self.args_parsed):
+                if sys_arg in arg_parsed.valid_flags:
                     # Store value as True
                     if arg_parsed.store_true:
                         arg_parsed.arg_value = True
                     # Store value as str
                     else:
-                        try:
-                            arg_parsed.arg_value = self.sys_args[i+1]
-                            # Argument value is possibly just a flag for another argument
-                            if arg_parsed.arg_value[0] == "-":
-                                raise IndexError
-                        except IndexError:
-                            sys.exit("[Arger] Argument " + sys_arg + " expects a value!")
+                        arg_parsed.arg_value = []
+                        for next_argument_after_flag in self.sys_args[i+1:]:
+                            # ...until the start of the next argument flag...
+                            if next_argument_after_flag[0] == "-":
+                                break
+                            arg_parsed.arg_value.append(next_argument_after_flag)
+
+                        if len(arg_parsed.arg_value) == 0:
+                            raise ArgumentException("[Arger] Argument " + sys_arg + " expects a value!")
 
     def readable(self):
         for arg in self.args_parsed:
-            print("\nName: {}\nStore true: {}\nHelp: {}\nValue: {}".format(arg.name, arg.store_true, arg.help, arg.arg_value))
+            print("\nValid Flags: {}\nStore true: {}\nHelp: {}\nValue: {}".format(list(arg.valid_flags), arg.store_true, arg.help, arg.arg_value))
+
+
+class ArgumentException(Exception):
+    pass
 
 
 class Argument:
-    name = ""
+    valid_flags = []
     store_true = False
     help = ""
     arg_value = None
-    def __init__(self, name, store_true, help):
-        self.name = name
+    def __init__(self, flags, store_true, help):
+        self.valid_flags = flags
         self.store_true = store_true
         self.help = help
 
