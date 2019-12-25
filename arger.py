a2ba1743409d87f2194354d83a28f5136c368402
JS <jandede@protonmail.com>
2019-12-25 20:49:06 +0300

    unnamed work, arrays work, booleans work

--- arger.py
+++ arger.py
@@ -22,23 +22,40 @@ class Arger:
             self.accepted_flags.append(a)
 
     def parse(self):
-        parsed_args = {}
+        found_args = {}
         sys_args_str = " ".join(self.sys_args[1:])
         # Save all arguments before the first dash as unnamed
-        unnamed_args = sys_args_str.split(" -")[0]
-        named_args = re.findall("(-{1,2}\w* *\w*)", sys_args_str)
+        unnamed = sys_args_str.split(" -")[0]
+        for ua in unnamed.split(" "):
+            self.unnamed_args.append(ua)
+        # Consider all that start with - or --, select the words that belong to
+        # that argument (that is words that appear before the next space+dash or eol)
+        named_args = re.findall("(-{1,2}.*?)(?= *-|$)", sys_args_str)
         for arg in named_args:
             flag = arg.strip().split(" ")[0]
             arg_values = arg.strip().split(" ")[1:]
-            parsed_args[flag] = arg_values
-        for arg in parsed_args.keys():
+            found_args[flag] = arg_values
+        # Validate and add values to Argument objects in args_parsed
+        for arg in found_args.keys():
             # TODO even I don't know what this does tomorrow
             if arg not in " ".join([" ".join(x.valid_flags) for x in self.args_parsed]).split(" "):
                 raise ArgumentException("Argument " + arg + " has not been specified in this program!")
-            # elif [] and parsed_args[arg] != []:
-            #     raise ArgumentException("Argument " + arg + " should not have argument values! (it is just a flag)")
+            else:
+                for parsed_arg in self.args_parsed:
+                    if arg in parsed_arg.valid_flags and parsed_arg.store_true and found_args[arg] != []:
+                        raise ArgumentException("Argument " + arg + " stores true and does not expect a value!")
+                    elif arg in parsed_arg.valid_flags and not parsed_arg.store_true and found_args[arg] == []:
+                            raise ArgumentException("Argument " + arg + " expects a value!")
+                    elif arg in parsed_arg.valid_flags and parsed_arg.store_true:
+                        parsed_arg.arg_value = True
+                    elif arg not in parsed_arg.valid_flags and parsed_arg.store_true:
+                        parsed_arg.arg_value = False
+                    elif arg in parsed_arg.valid_flags and not parsed_arg.store_true:
+                        parsed_arg.arg_value = found_args[arg]
 
     def readable(self):
+        if self.unnamed_args:
+            print("Unnamed args:", self.unnamed_args)
         for arg in self.args_parsed:
             print("\nValid Flags: {}\nStore true: {}\nHelp: {}\nValue: {}".format(list(arg.valid_flags), arg.store_true, arg.help, arg.arg_value))
 
