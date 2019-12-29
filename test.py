3ec89823710973a3350286346c31caf429633d8f
JS <jandede@protonmail.com>
2019-12-29 10:43:56 +0300

    test script modified for later revisions

--- test.py
+++ test.py
@@ -2,8 +2,10 @@
 import arger
 
 ap = arger.Arger()
-ap.add_arg("append", "-a", "abc")
-ap.add_arg("test1", "--delete", "-d", required=True)
+ap.add_positional_arg("files", arg_type=list, help="Files that you want selected", required=True)
+ap.add_arg("append", "-a", "--append", help="Use this flag to append files")
+ap.add_arg("test1", "--delete", "-d", arg_type=list, required=True, help="Use this flag to delete files")
 ap.add_arg("test2", "-f", store_true=True, help="Test")
 ap.parse()
 ap.readable()
+
